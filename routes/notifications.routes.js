609d186af6d7768a9c439de259c1534b94950eef
Farid Machrouh <anubis_immortel@hotmail.com>
2025-07-09 11:30:34 +0200

    Correction du chemin du middleware d'authentification pour les notifications

--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Notification = require("../models/Notification");
 const User = require("../models/user.model");
-const verifyToken = require("../middlewares/authMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
 
 // ✅ INSCRIPTION à la newsletter (protégé par JWT)
 router.post("/", verifyToken, async (req, res) => {
